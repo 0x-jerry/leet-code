35a6ef8a620bb5c9e6f7c19ca805ead269223b29
cwxyz007 <jie844067636@gmail.com>
2019-06-12 09:47:11 +0800

    2019-6-12 09:47:07

---
+++ 26.remove-duplicates-from-sorted-array.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=26 lang=javascript
+ *
+ * [26] Remove Duplicates from Sorted Array
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+  let pre = nums[0]
+
+  for (let i = 1; i < nums.length; ) {
+    const n = nums[i]
+    if (pre === n) {
+      nums.splice(i, 1)
+    } else {
+      pre = n
+      i++
+    }
+  }
+
+  return nums.length
+}
