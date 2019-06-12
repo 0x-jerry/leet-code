0a793057c007a8baf4bf6f951f1778c6bb451667
Fantasy <jie844067636@gmail.com>
2019-06-12 22:37:05 +0800

    2019-6-12 22:37:04

--- 38.count-and-say.js
+++ 38.count-and-say.js
@@ -8,6 +8,29 @@
  * @return {string}
  */
 var countAndSay = function(n) {
-    
-};
+  if (n === 1) {
+    return '1'
+  }
 
+  let lastSay = countAndSay(n - 1)
+
+  let say = ''
+
+  let char = lastSay[0]
+  let count = 1
+
+  for (let i = 1; i < lastSay.length; i++) {
+    const c = lastSay[i]
+    if (char === c) {
+      count++
+    } else {
+      say += count + char
+      char = c
+      count = 1
+    }
+  }
+
+  say += count + char
+
+  return say
+}
