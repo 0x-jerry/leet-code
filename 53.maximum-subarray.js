bd6da1d086af8219ae78588fa752e905f8acd106
Fantasy <jie844067636@gmail.com>
2019-06-22 12:48:31 +0800

    2019-6-22 12:48:30

--- 53.maximum-subarray.js
+++ 53.maximum-subarray.js
@@ -2,12 +2,40 @@
  * @lc app=leetcode id=53 lang=javascript
  *
  * [53] Maximum Subarray
+ *
+ * solution 1
+ *
+ * Calculate every subarray max sum value and return the max one
+ * F(n) = Fsum(n) - Fminsum(n-1)
  */
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var maxSubArray = function(nums) {
+  let sum = 0
+  let minSum = 0
+  let maxSub = -Infinity
+
+  nums.forEach((n) => {
+    minSum = Math.min(minSum, sum)
+
+    sum += n
+
+    maxSub = Math.max(maxSub, sum - minSum)
+  })
+
+  return maxSub
+}
+
+/**
+ * solution 0
+ *
+ * Abandon the subarray which sum <= 0
+ *
+ * @param {number[]} nums
+ */
+const solution0 = function(nums) {
   let result = -Infinity
 
   let sum = 0
