7ecba8f85b904fae16dd3b3d4d1cb447257ae163
0x-jerry <x.jerry.wang@gmail.com>
2021-10-27 16:16:02 +0800

    chore: update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,17 @@
 import { Config } from '@jest/types'
 
 const conf: Config.InitialOptions = {
+  preset: 'ts-jest',
   testMatch: [
     //
     '**/src/**/*.ts',
     '**/*.test.ts',
   ],
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
 }
 
 export default conf
