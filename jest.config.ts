bb8746887346ab780446246a2087a67fc1f3617b
0x-jerry <x.jerry.wang@gmail.com>
2021-10-27 16:30:12 +0800

    Revert "chore: update jest config"
    This reverts commit 7ecba8f85b904fae16dd3b3d4d1cb447257ae163.

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,11 @@
 import { Config } from '@jest/types'
 
 const conf: Config.InitialOptions = {
-  preset: 'ts-jest',
   testMatch: [
     //
     '**/src/**/*.ts',
     '**/*.test.ts',
   ],
-  globals: {
-    'ts-jest': {
-      isolatedModules: true,
-    },
-  },
 }
 
 export default conf
