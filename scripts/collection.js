3cd310d0ebd98185c97d8fe4c79757f3a8232fcf
Jerry Wang <x.jerry.wang@gmail.com>
2021-11-04 22:16:23 +0800

    chore: update statistics script

--- scripts/collection.js
+++ scripts/collection.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const path = require('path')
 
 function getJSFileCount() {
-  const dir = path.join(__dirname, '..')
-  return fs.readdirSync(dir).filter((v) => v.endsWith('.js')).length
+  const dir = path.join(__dirname, '..', 'src')
+  return fs.readdirSync(dir).filter((v) => /\.[tj]s$/.test(v)).length
 }
 
 console.log(getJSFileCount())
