cebfef400ede1c063f34bc81e5c957ea96dbc83c
Jerry Wang <x.jerry.wang@gmail.com>
2022-01-08 16:47:20 +0800

    824

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod s812_largest_triangle_area;
 pub mod s819_most_common_word;
 pub mod s821_shortest_distance_to_a_character;
+pub mod s824_goat_latin;
---
+++ src/s824_goat_latin.rs
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode id=824 lang=rust
+ *
+ * [824] Goat Latin
+ */
+
+pub struct Solution {}
+
+// @lc code=start
+impl Solution {
+  pub fn to_goat_latin(sentence: String) -> String {
+    let vowels: Vec<char> = vec!['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    let words = sentence.split_whitespace();
+    let mut result: String = String::new();
+    let letter_a = String::from("a");
+
+    let mut idx = 0;
+    for word in words {
+      let first = word.chars().next().unwrap();
+
+      if !result.is_empty() {
+        result.push(' ')
+      }
+
+      if vowels.contains(&first) {
+        result.push_str(word);
+        result.push_str("ma");
+      } else {
+        result.push_str(word.get(1..).unwrap());
+        result.push(first);
+        result.push_str("ma");
+      };
+
+      result.push_str(letter_a.repeat(idx + 1).as_str());
+
+      idx = idx + 1;
+    }
+
+    return result;
+  }
+}
+// @lc code=end
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test1() {
+    assert_eq!(
+      "Imaa peaksmaaa oatGmaaaa atinLmaaaaa".to_string(),
+      Solution::to_goat_latin("I speak Goat Latin".to_string())
+    );
+  }
+}
