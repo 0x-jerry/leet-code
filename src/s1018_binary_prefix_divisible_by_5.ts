04c87710b9610240a83dd2c5ebb86e697c098935
0x-jerry <x.jerry.wang@gmail.com>
2022-04-13 10:10:56 +0800

    1018

---
+++ src/s1018_binary_prefix_divisible_by_5.ts
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode id=1018 lang=typescript
+ *
+ * [1018] Binary Prefix Divisible By 5
+ */
+
+// @lc code=start
+function prefixesDivBy5(nums: number[]): boolean[] {
+  const result: boolean[] = []
+
+  const divisibleBy = 5
+  let remainder = 0
+
+  for (const bit of nums) {
+    remainder = remainder << 1
+    remainder = remainder | bit
+
+    if (remainder >= divisibleBy) {
+      remainder = remainder - divisibleBy
+    }
+
+    result.push(remainder === 0)
+  }
+
+  return result
+}
+// @lc code=end
+
+describe('prefixes div by 5', () => {
+  it('nums = [0,1,1]', () => {
+    expect(prefixesDivBy5([0, 1, 1])).toEqual([true, false, false])
+  })
+
+  it('nums = [1,1,1]', () => {
+    expect(prefixesDivBy5([1, 1, 1])).toEqual([false, false, false])
+  })
+
+  it('nums = [1,0,0,1,0,1,0,0,1,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1,0,1,0,0,0,0,1,1,0,1,0,0,0,1]', () => {
+    expect(
+      prefixesDivBy5([
+        1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0,
+        0, 1,
+      ])
+    ).toEqual([
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      false,
+      true,
+      false,
+      false,
+      true,
+      true,
+      true,
+      true,
+      false,
+    ])
+  })
+})
