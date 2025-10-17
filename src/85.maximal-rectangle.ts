76e62659b0d585a1b6947814503ccf960d7b9dba
Jerry <x.jerry.wang@gmail.com>
2025-10-17 06:11:50 +0000

    wip: 85

---
+++ src/85.maximal-rectangle.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=85 lang=typescript
+ *
+ * [85] Maximal Rectangle
+ */
+
+// @lc code=start
+function maximalRectangle(matrix: string[][]): number {
+    
+};
+// @lc code=end
+
