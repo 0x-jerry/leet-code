eb6f8ad45370db47118f74ead8de92433a2e1e74
Jerry <x.jerry.wang@gmail.com>
2025-10-17 06:10:15 +0000

    1411

--- src/1411.number-of-ways-to-paint-n-3-grid.ts
+++ src/1411.number-of-ways-to-paint-n-3-grid.ts
@@ -6,13 +6,32 @@
 
 // @lc code=start
 function numOfWays(n: number): number {
-  return 0
+  const maxValue = 10 ** 9 + 7
+
+  // a -> 3a + 2b
+  // b -> 2a + 2b
+
+  let result = [6, 6]
+  let p = [...result]
+  for (let i = 1; i < n; i++) {
+    p = result
+
+    const a = (p[0] * 3 + p[1] * 2) % maxValue
+    const b = (p[0] * 2 + p[1] * 2) % maxValue
+    result = [a, b]
+  }
+
+  return (result[0] + result[1]) % maxValue
 }
 // @lc code=end
 
 describe('Number of Ways to Paint N × 3 Grid', () => {
   it('n = 1', () => {
     expect(numOfWays(1)).toBe(12)
+  })
+
+  it('n = 2', () => {
+    expect(numOfWays(2)).toBe(54)
   })
 
   it('n = 5000', () => {
