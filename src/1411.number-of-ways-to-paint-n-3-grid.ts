a1231a17ebfbe185d5d744bbf9f6e7103966259a
Jerry <x.jerry.wang@gmail.com>
2025-10-17 02:17:14 +0000

    wip: 1411

---
+++ src/1411.number-of-ways-to-paint-n-3-grid.ts
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=1411 lang=typescript
+ *
+ * [1411] Number of Ways to Paint N × 3 Grid
+ */
+
+// @lc code=start
+function numOfWays(n: number): number {
+  return 0
+}
+// @lc code=end
+
+describe('Number of Ways to Paint N × 3 Grid', () => {
+  it('n = 1', () => {
+    expect(numOfWays(1)).toBe(12)
+  })
+
+  it('n = 5000', () => {
+    expect(numOfWays(5000)).toBe(30228214)
+  })
+})
