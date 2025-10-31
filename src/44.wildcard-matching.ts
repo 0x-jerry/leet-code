4345c7cd7225a72465b81790f5d6aa1edc9ffdc4
Jerry <x.jerry.wang@gmail.com>
2025-10-31 10:29:55 +0000

    44

--- src/44.wildcard-matching.ts
+++ src/44.wildcard-matching.ts
@@ -6,150 +6,52 @@
 
 // @lc code=start
 function isMatch(s: string, p: string): boolean {
-  const patten = _normalize(p)
+  p = p.replace(/\*+/g, '*')
 
-  const _cache: (boolean | undefined)[][] = Array.from({ length: s.length + 1 }, () =>
-    new Array(patten.length + 1).fill(undefined),
-  )
+  const lenP = p.length
+  const lenS = s.length
 
-  if (!s && patten.length === 1 && patten[0] === '*') {
-    return true
-  }
+  let i = 0
+  let j = 0
 
-  return _isMatch(0, 0)
+  let backtraceStarPos = -1
+  let matched = 0
 
-  function setCache(i: number, p: number, v: boolean) {
-    _cache[i][p] = v
-  }
+  while (i < lenS) {
+    if (j < lenP && (p[j] === '?' || s[i] === p[j])) {
+      i++
+      j++
+    } else if (j < lenP && p[j] === '*') {
+      backtraceStarPos = j
+      matched = i
 
-  function getCache(i: number, p: number) {
-    return _cache[i][p]
-  }
+      j++
+    } else if (backtraceStarPos !== -1) {
+      j = backtraceStarPos + 1
 
-  function _isMatch(strStartIdx: number, pIdx: number): boolean {
-    if (strStartIdx >= s.length && pIdx >= patten.length) {
-      return true
-    }
-
-    if (strStartIdx >= s.length) {
-      return patten.slice(pIdx).every((x) => x === '*')
-    }
-
-    if (pIdx >= patten.length) {
-      return false
-    }
-
-    const result = getCache(strStartIdx, pIdx)
-    if (result != null) {
-      return result
-    }
-
-    const pChars = patten[pIdx]
-
-    if (pChars === '*') {
-      let count = 0
-
-      let chars = ''
-
-      while (pIdx < patten.length - 1) {
-        //
-        pIdx++
-        if (patten[pIdx] === '?') {
-          count++
-        } else if (patten[pIdx] !== '*') {
-          chars = patten[pIdx]
-          break
-        }
-      }
-
-      if (chars === '') {
-        return s.slice(strStartIdx).length >= count
-      }
-
-      let matched = false
-      const charsLen = chars.length
-
-      while (strStartIdx < s.length) {
-        if (chars === s.slice(strStartIdx, strStartIdx + charsLen)) {
-          if (count <= 0) {
-            matched = _isMatch(strStartIdx, pIdx)
-
-            setCache(strStartIdx, pIdx, matched)
-
-            if (matched) {
-              return matched
-            }
-          }
-        }
-
-        count--
-        strStartIdx++
-      }
-
-      return false
-    } else if (pChars === '?') {
-      const result = _isMatch(strStartIdx + 1, pIdx + 1)
-      setCache(strStartIdx + 1, pIdx + 1, result)
-
-      return result
+      matched++
+      i = matched
     } else {
-      const chars = s.slice(strStartIdx, strStartIdx + pChars.length)
-
-      if (pChars === chars) {
-        const result = _isMatch(strStartIdx + pChars.length, pIdx + 1)
-
-        setCache(strStartIdx + pChars.length, pIdx + 1, result)
-
-        return result
-      }
+      return false
     }
-
-    return false
   }
 
-  function _normalize(p: string) {
-    const patten: string[] = []
-
-    let current = ''
-    for (let i = 0; i < p.length; i++) {
-      const char = p[i]
-
-      if (char === '*') {
-        if (current === '*') {
-          continue
-        } else if (current.length) {
-          patten.push(current)
-          current = ''
-        }
-        current = '*'
-      } else if (char === '?') {
-        if (current) {
-          patten.push(current)
-        }
-        patten.push(char)
-        current = ''
-      } else {
-        if (current === '*') {
-          patten.push(current)
-          current = ''
-        }
-
-        current += char
-      }
-    }
-
-    if (current) {
-      patten.push(current)
-    }
-
-    return patten
+  while (j < lenP && p[j] === '*') {
+    j++
   }
+
+  return j === lenP
 }
+
 // @lc code=end
 
 describe('Wildcard Matching', () => {
   it('s = "aa", p = "a"', () => {
     expect(isMatch('aa', 'a')).toBe(false)
+  })
+
+  it('s = "hi", p = "*?"', () => {
+    expect(isMatch('hi', '*?')).toBe(true)
   })
 
   it('s = "aa", p = "aa"', () => {
