ea1d55fdaf8be3a5f0b721fb85512487e12b7dad
Jerry <x.jerry.wang@gmail.com>
2025-10-31 06:54:24 +0000

    chore: WIP 44

---
+++ src/44.wildcard-matching.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=44 lang=typescript
+ *
+ * [44] Wildcard Matching
+ */
+
+// @lc code=start
+function isMatch(s: string, p: string): boolean {
+    
+};
+// @lc code=end
+
